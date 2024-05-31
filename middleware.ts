08bf3d047552e87c75a12fcc8dacc5a1ecc047e8
Joe Kariuki <jknguru6@gmail.com>
2024-05-31 06:47:53 +0300

    chore: Update clerkMiddleware to handle public routes

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/(.*)"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
-  const { userId, redirectToSignIn } = auth();
-
-  // If user isn't sign in and is trying to access a protected route, redirect them to sign in
-  if (!userId && isProtectedRoute(req)) {
-    return redirectToSignIn({ returnBackUrl: req.url });
+  if (!isPublicRoute(req)) {
+    auth().protect();
   }
-
-  // If user is signed in and route is proteced, allow access
-  if (userId && isProtectedRoute(req)) return NextResponse.next();
 });
 
 export const config = {
