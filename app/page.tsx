422a67324d159c332dc738e364f147385ea9e486
Joe Kariuki <jknguru6@gmail.com>
2024-05-31 06:08:29 +0300

    chore: Add UserButton component for Clerk integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <h1>Home</h1>
+      <UserButton />
     </div>
   );
 }
