eae914f940bf8756fde070adf9a8457bf0c67ed4
Joe Kariuki <jknguru6@gmail.com>
2024-05-31 06:52:37 +0300

    refactor: Update page layout to include flex column for home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <h1>Home</h1>
       <UserButton />
     </div>
