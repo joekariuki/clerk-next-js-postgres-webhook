909a628b31f23491d74184ec37f14d45dba4c75e
Joe Kariuki <jknguru6@gmail.com>
2024-05-31 06:12:36 +0300

    chore: Update clerkMiddleware to handle protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(["/(.*)"]);
+
+export default clerkMiddleware((auth, req) => {
+  const { userId, redirectToSignIn } = auth();
+
+  // If user isn't sign in and is trying to access a protected route, redirect them to sign in
+  if (!userId && isProtectedRoute(req)) {
+    return redirectToSignIn({ returnBackUrl: req.url });
+  }
+
+  // If user is signed in and route is proteced, allow access
+  if (userId && isProtectedRoute(req)) return NextResponse.next();
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
