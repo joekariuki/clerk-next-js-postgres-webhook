e33964a8bf15dcc681654080d4028d6aa4a6269d
Joe Kariuki <jknguru6@gmail.com>
2024-06-02 06:40:18 +0300

    feat: Update home page to use Link component for navigation to dashboard page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1>Home</h1>
-      <UserButton />
+      <Link href="/dashboard">Go to dashboard page</Link>
     </div>
   );
 }
