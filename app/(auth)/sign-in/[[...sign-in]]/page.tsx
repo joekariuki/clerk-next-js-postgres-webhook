5ce88fca9dde0fe6439c35e2de38d3c337d5ca90
Joe Kariuki <jknguru6@gmail.com>
2024-06-13 15:03:44 +0300

    refactor: Update sign-in page to redirect to dashboard if user is already signed in
    This commit updates the sign-in page to check if the user is already signed in using the @clerk/nextjs library. If the user is signed in, they will be redirected to the dashboard page. This improves the user experience by preventing signed-in users from accessing the sign-in page unnecessarily.

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,27 @@
-import { SignIn } from "@clerk/nextjs";
 
-export default function Page() {
-  return <SignIn />;
+import prismadb from "@/lib/prismadb";
+import { SignIn } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+
+
+export default async function Page() {
+ 
+
+  const clerkUser = await currentUser();
+  const clerkUserId = clerkUser?.id;
+
+
+
+  const user =  await prismadb.user.findFirst({
+    where: {
+        clerkId: clerkUserId
+    }
+  });
+
+  if (!user) {
+    return;
+  } 
+
+  
+  return <SignIn forceRedirectUrl={`/dashboard/${user.id}`} />;
 }
