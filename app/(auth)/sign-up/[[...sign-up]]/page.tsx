def2ddfe9b1424882b397f1372e3ae6dba65b712
Joe Kariuki <jknguru6@gmail.com>
2024-06-13 15:04:01 +0300

    refactor: Update sign-up page to fetch user data from database
    This commit modifies the sign-up page to fetch user data from the database using the @clerk/nextjs and prismadb libraries. It retrieves the user's ID from the clerkUser object and uses it to query the database for the corresponding user. If the user is found, the sign-up form is rendered, otherwise, null is returned. This improves the sign-up process by ensuring that only existing users can access the sign-up page.

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,28 @@
-import { SignUp } from "@clerk/nextjs";
 
-export default function Page() {
-  return <SignUp />;
+import prismadb from "@/lib/prismadb";
+import { SignUp } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+
+
+export default async function Page() {
+  
+
+  const clerkUser = await currentUser();
+  const clerkUserId = clerkUser?.id;
+
+
+  const user =  await prismadb.user.findFirst({
+    where: {
+        clerkId: clerkUserId
+    }
+  });
+
+  if (!user) {
+    return null;
+  }
+    
+  
+  return <SignUp fallbackRedirectUrl={`/dashboard/${user.id}`} signInFallbackRedirectUrl="/sign-in" />;
+
+
 }
